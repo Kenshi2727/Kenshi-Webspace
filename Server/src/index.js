f6ec2f8350771baf84921ba3cc9d652cdc169b20
Abhishek Mathur <abhishekmathurofficial@gmail.com>
2025-09-02 23:00:36 +0530

    corrected middleware order

--- Server/src/index.js
+++ Server/src/index.js
@@ -12,11 +12,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 //middlewares
-// sending raw buffer to /users/create instead of json as webhook verify expects raw buffer
-app.use("/users", bodyParser.raw({ type: "application/json" }), userRoutes);
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(cors(
     {
         origin: process.env.CORS_ORIGIN,
@@ -24,7 +19,12 @@ app.use(cors(
         credentials: true,
     }
 ));
+app.use(express.urlencoded({ extended: true }));
 app.use(clerkMiddleware());
+// sending raw buffer to /users/create instead of json as webhook verify expects raw buffer
+app.use("/users", bodyParser.raw({ type: "application/json" }), userRoutes);
+app.use(express.json());
+// rest middlewares for json type
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
