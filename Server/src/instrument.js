b3a3bb37844c41228452fad43adb5475c6bd2126
Abhishek Mathur <abhishekmathurofficial@gmail.com>
2025-11-28 23:23:20 +0530

    corrected env injection in server:sentry:instrument.js

--- Server/src/instrument.js
+++ Server/src/instrument.js
@@ -5,7 +5,7 @@ dotenv.config()
 
 Sentry.init({
     dsn: process.env.SENTRY_DSN,
-    environment: import.meta.env.MODE || "development",
+    environment: process.env.MODE || "development",
     // Setting this option to true will send default PII data to Sentry.
     // For example, automatic IP address collection on events
     sendDefaultPii: true,
