676d9931289e572ecf5934f12acf62fad5ecf366
Abhishek Mathur <abhishekmathurofficial@gmail.com>
2026-08-03 00:13:11 +0530

    dev:added mvc layers to user model

--- apps/content-service/database/README.md
+++
@@ -1,2 +0,0 @@
-# Kenshi Webspace - Database
- **Database Console** - [Production(Authorized!)](https://console.aiven.io/account/a55aaa94f37d/project/abhishekmathurofficial-9ce7/services/kenshi-webspace/overview)
--- apps/content-service/src/controllers/user.controller.js
+++ apps/content-service/src/controllers/user.controller.js
@@ -1,156 +1,42 @@
-import prisma from "@kenshi/database/prisma.client.js";
-import { clerkClient } from "@clerk/express";
-import { Webhook } from "svix";
-import dotenv from "dotenv";
+import * as controller from "../controllers/user.controller.js";
 
 dotenv.config();
 
-// helper functions
 const createUser = async (req, res, evt, id) => {
-    console.log("User creation initiated");
-    try {
-        const { email_addresses, primary_email_address_id, first_name, last_name } = evt.data;
-        console.log("Email Addresses:", email_addresses);
-        console.log("Primary Email Address ID:", primary_email_address_id);
+    const { status, message } = await controller.createUser(req, res, evt, id);
+    return res.status(status).json({ message });
+};
 
-        // Simulating an error for testing purposes
-        // throw new Error("Test error");
-
-        const primaryEmail = email_addresses.find(email => email.id === primary_email_address_id);
-
-        if (!primaryEmail) {
-            console.error("Primary email not found");
-            return res.status(400).send("Bad Request: Primary email not found");
-        }
-
-        //creating user in Database
-        await prisma.user.create({
-            data: {
-                id,
-                firstName: first_name,
-                lastName: last_name,
-                email: primaryEmail.email_address,
-            }
-        })
-        console.log("User created in database with ID:", id);
-    } catch (error) {
-        console.error("Error creating user in database:", error);
-        //deleting user from db
-        res.locals.userId = id;
-        // res.status(500).send("Internal Server Error: Failed to create user");
-        return deleteUser(req, res);
+const getUser = async (req, res) => {
+    const { status, message, user } = await controller.getUser(req, res);
+    if (user) {
+        return res.status(status).json({ user });
     }
-    return res.status(201).json({ message: "User created successfully" });
+    return res.status(status).json({ message });
+}
+
+const deleteUser = async (req, res) => {
+    const { status, message, data } = await controller.deleteUser(req, res);
+    if (data) {
+        return res.status(status).json({ message, data });
+    }
+    return res.status(status).json({ message });
 };
 
 const deleteUserWebhook = async (req, res, evt, id) => {
-    console.log("User deletion via webhook initiated");
-    try {
-        console.log("User ID to delete:", id);
-        //deleting user from db
-        await prisma.user.delete({
-            where: { id }
-        });
-        console.log("User deleted from database with ID:", id);
-        return res.status(200).json({ message: "User deleted successfully" });
-    } catch (error) {
-        console.error("Error deleting user from database:", error);
-        return res.status(500).json({ message: "Internal Server Error: Failed to delete user from database" });
-    }
+    const { status, message } = await controller.deleteUserWebhook(req, res, evt, id);
+    return res.status(status).json({ message });
 };
 
-
-// export functions
-export const handleWebhook = (req, res) => {
-    const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
-
-    if (!WEBHOOK_SECRET) {
-        throw new Error("CLERK_WEBHOOK_SECRET is not defined in environment variables");
-    }
-
-    const headerPayload = req.headers;
-    const svixId = headerPayload["svix-id"];
-    const svixTimestamp = headerPayload["svix-timestamp"];
-    const svixSignature = headerPayload["svix-signature"];
-
-    //validation
-    if (!svixId || !svixTimestamp || !svixSignature) {
-        console.log("User creation failed: Missing Svix headers");
-        return res.status(400).send("Bad Request: Missing required Svix headers");
-    }
-
-    const payload = JSON.parse(req.body.toString("utf-8"));
-    console.log("Payload received:", payload);
-
-    const wh = new Webhook(WEBHOOK_SECRET);// creating webhook
-
-    let evt;// webhook type event
-    try {
-        evt = wh.verify(req.body, {
-            "svix-id": svixId,
-            "svix-timestamp": svixTimestamp,
-            "svix-signature": svixSignature
-        });
-    } catch (error) {
-        console.error("Error verifying webhook:", error);
-        return res.status(400).send("Bad Request: Invalid Svix webhook");
-    }
-
-    const { id } = evt.data;
-    const eventType = evt.type;
-
-    //logs
-    console.log("Webhook event received:", evt);
-    console.log("Event ID:", id);
-    console.log("Event Type:", eventType);
-
-    switch (eventType) {
-        case "user.created": createUser(req, res, evt, id);
-            break;
-        case "user.deleted": deleteUserWebhook(req, res, evt, id);
-            break;
-        default:
-            console.log(`Unhandled event type: ${eventType}`);
-            return res.status(500).send(`Internal Server Error: Event type ${eventType} not handled`);
-    }
+const handleWebhook = (req, res) => {
+    const { status, message } = controller.handleWebhook(req, res);
+    return res.status(status).json({ message });
 };
 
-export const deleteUser = async (req, res) => {
-    console.log("User deletion initiated");
-    try {
-        const userId = res.locals.userId;
-
-        if (!userId) {
-            console.warn("⚠️ No userId provided in res.locals");
-            return res.status(400).json({ message: "User ID is required" });
-        }
-
-        const response = await clerkClient.users.deleteUser(userId);
-        console.log("User deleted:", response);
-        return res.status(200).json({
-            message: "User deleted successfully",
-            data: response
-        });
-    } catch (error) {
-        console.error("Error deleting user:", error);
-        return res.status(500).json({ message: "Internal Server Error: Failed to delete user" });
-    }
-};
-
-export const getUser = async (req, res) => {
-    console.log("User info request for id:", req.params.userId);
-    try {
-        const user = await prisma.user.findUnique({
-            where: { id: req.params.userId },
-        });
-        if (!user) {
-            console.warn("⚠️ User not found with ID:", req.params.userId);
-            return res.status(404).json({ message: "User not found[SECURITY BREACH!]" });
-        }
-        return res.status(200).json({ user });
-    } catch (error) {
-        console.error("Error retrieving user:", error);
-        return res.status(500).json({ message: "Internal Server Error: Failed to retrieve user" });
-    }
-
+export {
+    createUser,
+    getUser,
+    deleteUser,
+    deleteUserWebhook,
+    handleWebhook
 }
